0d9a80112c9e1e1c645c8f10172dc14f41f3ea16
Adrian Dvergsdal <github.com@konto.atmoz.no>
2015-02-26 22:15:52 +0100

    Upgrade docker-compose to 1.1.0

--- vagrant-provision.sh
+++ vagrant-provision.sh
@@ -6,7 +6,8 @@ if [ ! -a /root/docker-is-ready ]; then
     curl -sSL https://get.docker.com/ubuntu/ | sh
 
     # Install docker compose
-    curl -L https://github.com/docker/fig/releases/download/1.1.0-rc2/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose; chmod +x /usr/local/bin/docker-compose
+    curl -L https://github.com/docker/compose/releases/download/1.1.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+    chmod +x /usr/local/bin/docker-compose
 
     # Comfortable access to docker
     sudo usermod -a -G docker vagrant
